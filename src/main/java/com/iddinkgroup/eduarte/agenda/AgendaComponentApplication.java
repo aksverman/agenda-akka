f28281e30e84c1f455346c751d9ae05dbcd7ab77
ankushverman <ankush.verman@iddinkgroup.com>
2019-10-13 20:38:49 +0530

    Initial Commit with dependency

---
+++ src/main/java/com/iddinkgroup/eduarte/agenda/AgendaComponentApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.iddinkgroup.eduarte.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ankus
+ *
+ */
+@SpringBootApplication
+public class AgendaComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaComponentApplication.class, args);
+	}
+}
